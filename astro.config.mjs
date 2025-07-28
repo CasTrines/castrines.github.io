b9a8a1bd5142b3ebb9e80b3ab91f4116520d2c10
CasTrines <cas.trines1998@gmail.com>
2025-07-28 11:32:01 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://castrines.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
